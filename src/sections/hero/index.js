6ecf8ed0099d40e2684292552fc4d770eec243f6
Marco <stari.marco11@gmail.com>
2021-12-16 18:56:51 +0100

    Akronos Technologies & Electrolux update

--- src/sections/hero/index.js
+++ src/sections/hero/index.js
@@ -1,7 +1,5 @@
 import React from "react";
 import {Trans} from "react-i18next";
-import {Link} from "react-scroll";
-import {NAV_BAR_HEIGHT} from "../../core/constants";
 import HeroImage from "./heroImage";
 
 const Hero = () => {
@@ -17,12 +15,15 @@ const Hero = () => {
               </Trans>
             </h1>
             <h5>
-              <Trans i18nKey="hero.subtitle">I’m a front-end developer and I’m excited to help you build your project</Trans>
+              <Trans i18nKey="hero.subtitle">
+                I'd be delighted to introduce you to some of the projects I've worked on as a full stack developer
+              </Trans>
+              {/* <Trans i18nKey="hero.subtitle">I’m a front-end developer and I’m excited to help you build your project</Trans> */}
             </h5>
             <div className="hero__buttons">
-              <Link className="btn btn--primary" to="contact" smooth={true} offset={-NAV_BAR_HEIGHT} duration={500}>
+              {/* <Link className="btn btn--primary" to="contact" smooth={true} offset={-NAV_BAR_HEIGHT} duration={500}>
                 <Trans i18nKey="hero.primary_button">Let's chat</Trans>
-              </Link>
+              </Link> */}
               {/* <a className="btn" href="#">
                 <Trans i18nKey="hero.secondary_button">Download Cv</Trans>
               </a> */}

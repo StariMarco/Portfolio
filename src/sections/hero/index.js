222c28069e2476cc4fea82029f32637386e90da1
Marco <stari.marco11@gmail.com>
2021-12-17 17:58:17 +0100

    Added Browse Projects Btn

--- src/sections/hero/index.js
+++ src/sections/hero/index.js
@@ -1,5 +1,7 @@
 import React from "react";
 import {Trans} from "react-i18next";
+import {Link} from "react-scroll";
+import {NAV_BAR_HEIGHT} from "../../core/constants";
 import HeroImage from "./heroImage";
 
 const Hero = () => {
@@ -21,9 +23,9 @@ const Hero = () => {
               {/* <Trans i18nKey="hero.subtitle">I’m a front-end developer and I’m excited to help you build your project</Trans> */}
             </h5>
             <div className="hero__buttons">
-              {/* <Link className="btn btn--primary" to="contact" smooth={true} offset={-NAV_BAR_HEIGHT} duration={500}>
-                <Trans i18nKey="hero.primary_button">Let's chat</Trans>
-              </Link> */}
+              <Link className="btn btn--primary" to="projects" smooth={true} offset={-NAV_BAR_HEIGHT} duration={500}>
+                <Trans i18nKey="hero.primary_button">Explore projects</Trans>
+              </Link>
               {/* <a className="btn" href="#">
                 <Trans i18nKey="hero.secondary_button">Download Cv</Trans>
               </a> */}
